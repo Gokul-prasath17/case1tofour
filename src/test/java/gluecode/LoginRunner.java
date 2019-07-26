e6b57609beee715d8d7c291988969119577cc660
Gokul-prasath17 <32924902+Gokul-prasath17@users.noreply.github.com>
2019-07-26 11:22:58 +0530

    Add files via upload

---
+++ src/test/java/gluecode/LoginRunner.java
@@ -0,0 +1,14 @@
+package gluecode;
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features= "src/test/resources/Features/case4.feature",plugin="json:target/Loginreport117.json")
+//@CucumberOptions(features= "src/test/resources/Features/case3.feature",plugin="json:target/Loginreport28.json")
+
+public class LoginRunner {
+
+}
